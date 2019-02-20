4fdef3b6c3be75784d5992ae753761f301a58633
Santos <rfsantos1@labs.neit.edu>
2019-02-20 13:19:07 -0500

    first commit

---
+++ Assets/Scenes/Scripts/Network.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+    SocketIOComponent socket;
+    public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+        socket.On("talkback", OnTalkBack);
+        socket.On("spawn", OnSpawn);
+	}
+
+    private void OnConnected(SocketIOEvent obj)
+    {
+        Debug.Log("Connected to Server");
+        socket.Emit("sayhello");
+    }
+
+    private void OnTalkBack(SocketIOEvent obj)
+    {
+        Debug.Log("The Server Says Hello to us");
+    }
+
+    private void OnSpawn(SocketIOEvent obj)
+    {
+        Debug.Log("Player Spawned");
+        Instantiate(playerPrefab);
+    }
+}
