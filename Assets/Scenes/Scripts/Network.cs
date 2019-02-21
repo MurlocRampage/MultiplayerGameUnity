aaa8a18dd883c6ca0bba80468a86d56c7e96c6f6
Santos <rfsantos1@labs.neit.edu>
2019-02-21 15:07:35 -0500

    second commit

--- Assets/Scenes/Scripts/Network.cs
+++ Assets/Scenes/Scripts/Network.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using SocketIO;
 
 public class Network : MonoBehaviour {
-    SocketIOComponent socket;
+    static SocketIOComponent socket;
     public GameObject playerPrefab;
+
+    Dictionary<string, GameObject> players;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,9 @@ public class Network : MonoBehaviour {
         socket.On("open", OnConnected);
         socket.On("talkback", OnTalkBack);
         socket.On("spawn", OnSpawn);
+        socket.On("move", OnMove);
+
+        players = new Dictionary<string, GameObject>();
 	}
 
     private void OnConnected(SocketIOEvent obj)
@@ -28,7 +33,33 @@ public class Network : MonoBehaviour {
 
     private void OnSpawn(SocketIOEvent obj)
     {
-        Debug.Log("Player Spawned");
-        Instantiate(playerPrefab);
+        Debug.Log("Player Spawned" + obj.data);
+        var player = Instantiate(playerPrefab);
+        players.Add(obj.data["id"].ToString(), player);
+        Debug.Log(players.Count);
+    }
+
+    private void OnMove(SocketIOEvent obj)
+    {
+        //Debug.Log("Player Moving" + obj.data);
+        var id = obj.data["id"].ToString();
+        //Debug.Log(id);
+
+        var v = float.Parse(obj.data["v"].ToString().Replace("\"", ""));
+        var h = float.Parse(obj.data["h"].ToString().Replace("\"", ""));
+
+        players[id].GetComponent<PlayerMovementNetwork>().v = v;
+        players[id].GetComponent<PlayerMovementNetwork>().h = h;
+    }
+
+    static public void Move(float currentPosV, float currentPosH)
+    {
+        //Debug.Log("Send Position to Server " + VectorToJson(currentPos));
+        socket.Emit("move", new JSONObject(VectorToJson(currentPosV, currentPosH)));
+    }
+
+    public static string VectorToJson(float dirV, float dirH)
+    {
+        return string.Format(@"{{""v"":""{0}"",""h"":""{1}""}}", dirV, dirH);
     }
 }
